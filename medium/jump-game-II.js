451c8f6076dde841fc838d219b93693b2f2d0735
cjlangan <connorjameslangan@gmail.com>
2025-01-27 21:44:52 -0600

    jan27

--- medium/jump-game-II.js
+++ medium/jump-game-II.js
@@ -3,7 +3,19 @@
  * @return {number}
  */
 var jump = function(nums) {
+    nums[nums.length - 1] = 0;
 
+    for(let i = nums.length - 2; i >= 0; i--) {
+        let min = Infinity;
+        for(let j = nums[i] + i; j > i; j--) {
+            if(j < nums.length) {
+                min = Math.min(min, nums[j]);
+            }
+        }
+        nums[i] = min + 1;
+    }
+
+    return nums[0];
 };
 
 const test = [0];
